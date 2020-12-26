bd5a353ba743986858e190d7b4ba1a0db9ea6e41
Ben Styling <ben@climbcreative.co.uk>
2020-12-26 11:59:03 +0000

    Add hero animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,16 @@ export default function Header() {
     return (
         <header className="header">
             <Logo />
-            <a
-                className="button button--outline-black"
-                href="https://github.com/chrisnharvey/magicstack">
-                View on GitHub
-            </a>
+            <nav className="site-nav">
+                <a href="#get-started">Get started</a>
+                <a href="#features">Features</a>
+                <a href="#docs">Docs</a>
+                <a
+                    className="button button--outline-black"
+                    href="https://github.com/chrisnharvey/magicstack">
+                    View on GitHub
+                </a>
+            </nav>
         </header>
     )
 }
